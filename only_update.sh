76145c42e8311e8bc54b8704c68b33869c5d21c1
Daniel <crasher6969@gmail.com>
2025-06-03 11:48:15 +0000

    Auto-commit from branch dev. Updated file: /home/tms_master_1/orion_platform/.git/FETCH_HEAD

--- only_update.sh
+++ only_update.sh
@@ -6,6 +6,6 @@ helm upgrade zwierzeta . -n envoy-ingress -f values.yaml #globalny refresh
 sleep 5
 kubectl -n envoy-ingress rollout restart deployment envoy-ingress # dodanie strony na podstawie nowego zwierzakai
 #sleep 3
-#kubectl -n envoy-ingress delete pods -l app=landing-html # odswiezenie spisu tresci
+kubectl -n envoy-ingress delete pods -l app=landing-html # odswiezenie spisu tresci
 #sleep 3
 #kubectl -n envoy-ingress delete pods -l app=${zwierzak}-html
